ba978bf8f3ee24ffbf5087bed3ab8749515f3553
jmBaldano <baldanojohnmark@gmail.com>
2025-10-24 20:34:00 +0800

    update carousel nav

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -63,7 +63,7 @@
 </div>
     <!-- Slider controls -->
     <button type="button" class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer hover:bg-black/20 start-0 group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span class="inline-flex items-center justify-center w-10 h-10  group-focus:ring-white group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
             </svg>
@@ -71,7 +71,7 @@
         </span>
     </button>
     <button type="button" class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer hover:bg-black/20 end-0 group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:ring-4 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span class="inline-flex items-center justify-center w-10 h-10   dark:group-focus:ring-white group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
